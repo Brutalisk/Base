8a89b42e2d5be24097db134ad6bdc008c1a3ea84
Brutalisk <matuta64@gmail.com>
2022-10-25 17:10:48 +0300

    New files

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,5 @@ class LoginPageLocators():
     URL = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM =(By.CSS_SELECTOR, "#register_form")
+class ProductPageLocators:
+    ADD_TO_CART = (By.CSS_SELECTOR, "#add_to_basket_form")
