0fc5631bbef0ea4c76086a74039fb0a2aa2820bc
Brutalisk <matuta64@gmail.com>
2022-10-25 16:30:05 +0300

    New files

--- test_main_page.py
+++ test_main_page.py
@@ -17,8 +17,13 @@ def test_guest_can_go_to_login_url(browser): #login_page
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_url()
-def test_guest_can_go_to_login_form(browser): #login_page
+def test_guest_can_see_login_form(browser): #login_page
     link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_form()
+def test_guest_can_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
