385a3025b874e65ac3e53bfd29d522e87f7d0986
Brutalisk <matuta64@gmail.com>
2022-11-06 17:26:37 +0300

    New files

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,7 @@ class LoginPageLocators():
     REGISTER_FORM =(By.CSS_SELECTOR, "#register_form")
 class ProductPageLocators:
     ADD_TO_CART = (By.CSS_SELECTOR, "#add_to_basket_form")
+    TITLE_NAME = (By.CSS_SELECTOR, "#div.col-sm-6.product_main.h1")
+    CART_TITLE_NAME = (By.CSS_SELECTOR, "#div.alertinner.strong")
+    TITLE_PRICE = (By.CSS_SELECTOR, "#div.col-sm-6.product_main.p:nth-child(2)")
+    CART_PRICE = (By.XPATH, "//div/p/strong[text()]")
